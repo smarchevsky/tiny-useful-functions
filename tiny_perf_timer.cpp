79e39edd15d45dac6c44604898408b19e54f8193
smarchevsky <staseg1987@gmail.com>
2026-03-09 16:43:00 +0200

    Update tiny_perf_timer.cpp
    Added indentation for scope timer logging

--- tiny_perf_timer.cpp
+++ tiny_perf_timer.cpp
@@ -10,8 +10,11 @@ class Logger {
 #endif
 
     using clock = std::chrono::high_resolution_clock;
+    friend class ScopeTimer;
+
     FILE* m_file;
     std::chrono::time_point<clock> _start;
+    int _indentation;
 
 public:
     Logger()
@@ -25,9 +28,11 @@ public:
             fprintf(m_file, "[LOGGING START]\n");
             fflush(m_file);
         }
+        _indentation = 0;
         _start = clock::now();
     }
 
+    template <bool WithIdentation>
     void log(const char* format, ...)
     {
         if (!m_file)
@@ -36,6 +41,10 @@ public:
         va_list args;
         va_start(args, format);
         lock_stream(m_file);
+        if constexpr (WithIdentation) {
+            for (int i = 0; i < _indentation; ++i)
+                fputc(' ', m_file);
+        }
         vfprintf(m_file, format, args);
         fputc('\n', m_file);
         unlock_stream(m_file);
@@ -45,7 +54,7 @@ public:
     void logTimepoint(const char* str)
     {
         const float duration = std::chrono::duration<float>(clock::now() - _start).count();
-        log("Timepoint: [%s] %f", str, duration * 1000);
+        log<false>("Timepoint: [%s] %f", str, duration * 1000);
     }
 
     ~Logger()
@@ -77,14 +86,16 @@ public:
     ScopeTimer(const char* str)
         : _timerName(str)
     {
-        Logger::get().log("%s, start", _timerName);
+        Logger::get()._indentation += 2;
+        Logger::get().log<true>("%s, start", _timerName);
         _start = clock::now();
     }
 
     ~ScopeTimer()
     {
         const float duration = std::chrono::duration<float>(clock::now() - _start).count();
-        Logger::get().log("%s, finished %.3f ms", _timerName, duration * 1000);
+        Logger::get().log<true>("%s, finished %.3f ms", _timerName, duration * 1000);
+        Logger::get()._indentation -= 2;
     }
 
 #define TIME_POINT_RAII(str) ScopeTimer scopeTimer(str);
